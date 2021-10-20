2d2bc009c8613f5757e97cda5b42ab150e0cb96b
Bruno Silva <brs.rsilva@gmail.com>
2021-10-20 16:36:33 -0300

    progress bar working

--- src/components/circle-svg/index.tsx
+++ src/components/circle-svg/index.tsx
@@ -4,17 +4,19 @@ import React from 'react'
 // styles
 import style from './style.module.scss'
 
+// assets
+import { ReactComponent as ProgressBarImg } from '../../assets/svg/bg.svg'
+
 const CircleSvg: React.FC = () => {
   return (
     <div className={style.progressBar}>
       <div className={style.skill}>
         <div className={style.outer}>
           <div className={style.inner}>
-            <div className={`${style.bar} ${style.left}`}>
-              <div className={style.progress} />
-            </div>
+            <div id="number">65%</div>
           </div>
         </div>
+        <ProgressBarImg />
       </div>
     </div>
   )
