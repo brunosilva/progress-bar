66eef9267ad5de4c12d3318829e05437f2037d2c
Bruno Silva <brs.rsilva@gmail.com>
2021-10-21 10:46:17 -0300

    feat: :sparkles: control the progress

--- src/components/circle/index.tsx
+++ src/components/circle/index.tsx
@@ -12,10 +12,22 @@ interface IProps {
   completed: number
 }
 const Circle: React.FC<IProps> = ({ completed }) => {
+  // keyframes references -------------------------
+  //   empty    = 0% { stroke-dashoffset: 582; }
+  //   ten      = 9% { stroke-dashoffset: 531; }
+  //   twenty   = 22% { stroke-dashoffset: 480; }
+  //   thirty   = 35% { stroke-dashoffset: 429; }
+  //   fourty   = 48% { stroke-dashoffset: 378; }
+  //   sixty    = 61% { stroke-dashoffset: 327; }
+  //   seventy  = 74% { stroke-dashoffset: 276; }
+  //   eighty   = 87% { stroke-dashoffset: 225; }
+  //   hundred  = 100% { stroke-dashoffset: 174; }
+  // ----------------------------------------------
+
   console.log(completed)
   return (
     <div className={style.progressBar}>
-      <div className={style.skill}>
+      <div className={`${style.skill} ${style.hundred}`}>
         <div className={style.outer}>
           <div className={style.inner}>
             <div className={style.bgMiddle} id="number">
